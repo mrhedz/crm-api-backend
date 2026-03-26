15bf08c4424306da85c9cb0258e0ee6b54c3904d
Martín Hernández <martinhernandez@MacBook-Air-de-Martin.local>
2026-03-25 20:33:02 -0600

    db updated

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,19 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT) || 5432,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl:
+        process.env.NODE_ENV === "production"
+          ? { rejectUnauthorized: false }
+          : false,
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT) || 5432,
+    });
 
 export default pool;
